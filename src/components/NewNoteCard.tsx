cc431b965bcd2f2fde12c51429386bc7699f2ff4
Mariano <marianocapiliku@gmail.com>
2024-02-06 15:28:29 +0100

    Create modal to create a new note To create the modal I used a third library from RadixUI (Dialog component) Dialog componet allows to create a dialog or modal that is opened when clicking on triger such as a button. One of good thing on using RadixUI is that we can use all component without the styling, allowing us to costumize it as we want

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,11 +1,50 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+
 const NewNoteCard = () => {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3">
-      <span className="text-sm font-medium text-slate-200">add notes</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Record an audio note that will be converted to text automatically.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-200">add notes</span>
+        <p className="text-sm leading-6 text-slate-400">
+          Record an audio note that will be converted to text automatically.
+        </p>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50">
+          <Dialog.Content className="fixed overflow-hidden -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2  max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
+            <div className="flex flex-1 flex-col gap-3 p-5 ">
+              <span className="text-sm font-medium text-slate-300">
+                Add note
+              </span>
+              <p className="text-sm leading-6 text-slate-400">
+                Start by{" "}
+                <button className="font=medium text-lime-400 hover:underline">
+                  recording an audio note
+                </button>{" "}
+                or, if you prefer, just{" "}
+                <button className="font=medium text-lime-400 hover:underline">
+                  {" "}
+                  write a text
+                </button>
+                .
+              </p>
+            </div>
+
+            <button className="w-full bg-lime-400 py-4 text-center text-sm text-slate-300 outline-none font-medium group">
+              <span className="text-lime-950 outline-none font-medium hover:bg-lime-500">
+                {" "}
+                Add note
+              </span>{" "}
+            </button>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
