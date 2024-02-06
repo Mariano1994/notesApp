37015189b2c8746c33cf9613caabe637db53196a
Mariano <marianocapiliku@gmail.com>
2024-02-06 15:10:23 +0100

    Create button to delete the note

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { formatDistanceToNow } from "date-fns";
+import { X } from "lucide-react";
 
 interface NoteCardPros {
   note: {
@@ -21,7 +22,10 @@ const NoteCard = ({ note }: NoteCardPros) => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50">
-          <Dialog.Content className="fixed -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2  max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Content className="fixed overflow-hidden -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2  max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+            <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
+              <X className="size-5" />
+            </Dialog.Close>
             <div className="flex flex-1 flex-col gap-3 p-5 ">
               <span className="text-sm font-medium text-slate-300">
                 {formatDistanceToNow(note.date, {
@@ -30,6 +34,15 @@ const NoteCard = ({ note }: NoteCardPros) => {
               </span>
               <p className="text-sm leading-6 text-slate-400">{note.content}</p>
             </div>
+
+            <button className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group">
+              Do you want to
+              <span className="text-red-400 group-hover:underline">
+                {" "}
+                delete this note
+              </span>{" "}
+              ?
+            </button>
           </Dialog.Content>
         </Dialog.Overlay>
       </Dialog.Portal>
